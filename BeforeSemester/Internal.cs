e894002df5c94b37e927e084172046927f536bab
Desktop <Desktop@DESKTOP-PC>
2021-07-20 17:20:15 +0530

    Add project files.

---
+++ BeforeSemester/Internal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Internal_Access_Specifier
+{
+    class Internal
+    {
+        private int num1;
+        private int num2;
+
+        public void showSum()
+        {
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1+num2));
+        }
+
+        public int Number1
+        {
+            get
+            {
+                return num1;
+            }
+            set
+            {
+                num1 = value;
+            }
+        }
+
+        public int Number2
+        {
+            get
+            {
+                return num2;
+            }
+            set
+            {
+                num2 = value;
+            }
+        }
+
+    }
+}
---
+++ BeforeSemester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Internal_Access_Specifier;
+namespace BeforeSemester
+{
+    class Access
+    {
+        public string userName;
+
+        public void getAccess()
+        {
+            Console.WriteLine("Enter your name : ");
+            userName = Console.ReadLine();
+        }
+    }
+
+    class Display
+    {
+        Access a = new Access();
+        public void showInfo()
+        {   
+            a.getAccess();
+            Console.WriteLine("Hello {0}",a.userName);
+        }
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Display record = new Display();
+            record.showInfo();
+        }
+    }
+}
